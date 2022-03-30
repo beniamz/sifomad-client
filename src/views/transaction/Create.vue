853e6729e3c8d187de44a946ee850a7ff30a5a5e
Beni Solehudin <beni80.amz@gmail.com>
2022-03-30 17:13:51 +0700

    menambah script Add transaction

--- src/views/transaction/Create.vue
+++ src/views/transaction/Create.vue
@@ -1,12 +1,86 @@
 <template>
 <div class="container">
-    <h3>Form Add Transaction</h3>
-    <div class="card rounded shadow">
-        <div class="card-header"> 
-            <Router-link :to="{ name: 'transaction.index'}" class="btn btn-primary btn-sm">Kembali</Router-link>
-        </div>
-        <div class="card-body">
-            form add transaction
+    <h3>Form Create Transaction     
+        <span><Router-link :to="{ name: 'transaction.index'}" class="btn btn-secondary btn-sm ml-5">Kembali</Router-link></span>
+    </h3>
+    <div>
+        <div class="nav-align-top mb-4">
+            <ul class="nav nav-pills mb-3 nav-fill" role="tablist">
+                <li class="nav-item">
+                    <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-justified-profile" aria-controls="navs-pills-justified-profile" aria-selected="true">
+                        <i class="tf-icons bx bx-user"></i> Home
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-justified-home" aria-controls="navs-pills-justified-home" aria-selected="false">
+                        <i class="tf-icons bx bx-home"></i> Data Orang Tua/Wali
+                    </button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="nav-link" role="tab" data-bs-toggle="tab" data-bs-target="#navs-pills-justified-document" aria-controls="navs-pills-justified-document" aria-selected="false">
+                        <i class="tf-icons bx bx-message-square"></i> Dokumen Pendukung
+                    </button>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane fade active show" id="navs-pills-justified-profile" role="tabpanel">
+                     <div class="row">
+                        <div class="col-md">
+                            <form @submit.prevent="store()">
+                                <div class="row">
+                                    <div class="col">
+                                        <label class="col-sm-2 col-form-label" for="basic-default-title">Title</label>
+                                        <div class="col-sm-12">
+                                            <input type="text" class="form-control" id="basic-default-title" placeholder="Title" v-model="transaction.title" />
+                                            <div class="text-danger">
+                                                Validation message
+                                            </div>
+                                        </div>
+                                        <label class="col-sm-2 col-form-label" for="basic-default-amount">Amount</label>
+                                        <div class="col-sm-12">
+                                            <input type="number" class="form-control" id="basic-default-amount" placeholder="50000" v-model="transaction.amount" />
+                                            <div class="text-danger">
+                                                Validation message
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <label class="col-sm-2 col-form-label" for="basic-default-type">Type</label>
+                                        <div class="col-sm-12">
+                                            <select class="form-select" id="type" aria-label="Default select example" v-model="transaction.type">
+                                                <option selected="">- Piih -</option>
+                                                <option value="revenue">Revenue</option>
+                                                <option value="expense">Expense</option>
+                                            </select>
+                                            <div class="text-danger">
+                                                Validation message
+                                            </div>
+                                        </div>
+                                        <label class="col-sm-2 col-form-label" for="basic-default-timestamp">Time</label>
+                                        <div class="col-sm-12">
+                                            <input type="text" class="form-control" id="basic-default-timestamp" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.time" />
+                                            <div class="text-danger">
+                                                Validation message
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button type="submit" class="btn btn-primary mt-3 full-right">Tambah Data</button>
+                                </div>
+                            </form>
+                            </div>
+                        </div>   
+                </div>
+                
+                <div class="tab-pane fade" id="navs-pills-justified-home" role="tabpanel">
+                       
+                </div>
+                
+                <div class="tab-pane fade" id="navs-pills-justified-document" role="tabpanel">
+                
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -14,8 +88,45 @@
 </template>
 
 <script>
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
 export default {
-    
+    setup() {
+        //data binding
+        const transaction = reactive({
+            title: '',
+            amount: '',
+            type: '',
+            time: '',
+        });
+        
+        const validation = ref([]);
+
+        const router = useRouter();
+
+        function store() {
+                axios.post('http://localhost:8000/api/transaction',
+                transaction
+            )
+            .then(() => {
+                router.push({ 
+                    name: 'transaction.index' 
+                    });
+
+            }).catch((err) => {
+                validation.value = err.response.data
+            });            
+        }
+
+        return {
+            transaction,
+            validation,
+            router,
+            store,
+            }            
+    },       
 }
 </script>
 
--- src/views/transaction/Index.vue
+++ src/views/transaction/Index.vue
@@ -1,42 +1,47 @@
 <template>  
-
-<div class="container">
-     <h3>Transaction List</h3>
-    <div class="card rounded shadow">
-        <div class="card-header">                    
-            
-            <Router-link :to="{ name: 'transaction.create'}" class="btn btn-primary btn-sm">Tambah Data</Router-link>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive text-nowrap">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>Amount</th>
-                            <th>Type</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">                      
-                        <tr v-for="(transaction, index) in transactions.data" :key="index">
-                            <td>{{ transaction.title }}</td>
-                            <td>{{ transaction.amount }}</td>
-                            <td>{{ transaction.type}}</td>
-                            <td>
-                                <div class="btn-group">
-                                    <Router-link :to="{ name: 'transaction.edit', params:{id: transaction.id},}" class="btn btn-sm btn-outline-info">Edit
-                                    </Router-link>
-                                    <button class="btn btn-sm btn-outline-danger">Delete</button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+    <div class="container">
+        <h3>Transaction List</h3>
+        <div class="card rounded shadow">
+            <div class="card-header">                    
+                
+                <Router-link :to="{ name: 'transaction.create'}" class="btn btn-primary btn-sm">Tambah Data</Router-link>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive text-nowrap">
+                    <table class="table table-hover">
+                        <thead class="dark">
+                            <tr>
+                                <th>Nama</th>
+                                <th>Amount</th>
+                                <th>Type</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">                      
+                            <tr v-for="(transaction, index) in transactions.data" :key="index">
+                                <td>{{ transaction.title }}</td>
+                                <td>{{ transaction.amount }}</td>
+                                <td>{{ transaction.type}}</td>
+                                <td>
+                                    <div class="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow text-primary" data-bs-toggle="dropdown"> Aksi
+                                        <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <Router-link :to="{ name: 'transaction.edit', params:{id: transaction.id},}" class="dropdown-item text-info" href="javascript:void(0);"
+                                                ><i class="bx bx-edit-alt me-1"></i> Edit</Router-link>
+                                            <a class="dropdown-item text-danger" href="javascript:void(0);"
+                                                ><i class="bx bx-trash me-1 "></i> Delete</a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-</div>  
+    </div>  
 </template>
 
 <script>
@@ -65,15 +70,5 @@ export default {
     }    
 }
 </script>
-
-<style>
-@media (min-width: 1024px) {
-  .index {
-    min-height: 100vh;
-    display: flex;
-  
-  }
-}
-</style>
 
 
